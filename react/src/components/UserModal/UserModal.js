b3cc99fcc6be0d83a18daa6a740275f3c9b6e1ea
hienpham <hien.pham@axonactive.vn>
2017-06-22 17:40:13 +0700

    -add css

--- react/src/components/UserModal/UserModal.js
+++ react/src/components/UserModal/UserModal.js
@@ -9,7 +9,24 @@ class UserModal extends React.Component {
 	  contentLabel="Modal"
 	>
 	  <h1>Modal Content</h1>
-	  <p>Etc.</p>
+	  <div>
+	  <form action="/action_page.php">
+	    <label for="fname">First Name</label>
+	    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+
+	    <label for="lname">Last Name</label>
+	    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+
+	    <label for="country">Country</label>
+	    <select id="country" name="country">
+	      <option value="australia">Australia</option>
+	      <option value="canada">Canada</option>
+	      <option value="usa">USA</option>
+	    </select>
+	  
+	    <input type="submit" value="Submit"/>
+	  </form>
+		</div>
 	</Modal>
   	}
 }
