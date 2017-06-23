2357223100f38e9af802c42deec909f9c0925206
hienpham <hien.pham@axonactive.vn>
2017-06-23 18:02:32 +0700

    - change modal

--- react/src/components/UserModal/UserModal.js
+++ react/src/components/UserModal/UserModal.js
@@ -1,34 +1,94 @@
 import React from 'react'
-import Modal from 'react-modal';
+import { Modal, Button, Form, FormGroup, Col, FormControl, ControlLabel, Table, Panel } from 'react-bootstrap'
 
+let fields = [
+	'name', 'address', 'phone', 'birthday', 'numberUsed', 'note'
+]
+
+let labels = {
+	name : 'Ten khach hang',
+	address : 'Dia chi',
+	phone : 'So dien thoai',
+	birthday : 'Ngay sinh',
+	numberUsed : 'So nguoi dung',
+	note : 'Ghi chu'
+}
 class UserModal extends React.Component {
 
+	contentPanel() {
+		return
+		(<Panel>
+			{this.tableContent()}
+		</Panel>)
+	}
+
+	tableContent() {
+		return (<Table striped bordered condensed hover>
+	    <thead>
+	      <tr>
+	        <th>#</th>
+	        <th>Dich vu</th>
+	        <th>Goi cuoc</th>
+	        <th>Gia cuoc</th>
+	        <th>Thoi gian su dung</th>
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr>
+	        <td>1</td>
+	        <td>Mark</td>
+	        <td>Otto</td>
+	        <td>@mdo</td>
+	        <td>@mdo</td>
+	      </tr>
+	      <tr>
+	        <td>2</td>
+	        <td>Jacob</td>
+	        <td>Thornton</td>
+	        <td>@fat</td>
+	        <td>@mdo</td>
+	      </tr>
+	      <tr>
+	        <td>3</td>
+	        <td colSpan="2">Larry the Bird</td>
+	        <td>@twitter</td>
+	        <td>@mdo</td>
+	      </tr>
+	    </tbody>
+	  </Table>)
+	}
+
 	render () {
-    return <Modal
-	  isOpen={this.props.modalIsOpen}
-	  contentLabel="Modal"
-	>
-	  <h1>Modal Content</h1>
-	  <div>
-	  <form action="/action_page.php">
-	    <label for="fname">First Name</label>
-	    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+     return (
+	    <div className="static-modal">
+		    <Modal show={this.props.modalIsOpen}>
+		      <Modal.Header>
+		        <Modal.Title>Modal title</Modal.Title>
+		      </Modal.Header>
 
-	    <label for="lname">Last Name</label>
-	    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+		      <Modal.Body>
+		        <Form horizontal>
+		        	{fields.map(field =>(<FormGroup key={field} controlId={"formHorizontal" + field }>
+					      <Col componentClass={ControlLabel} sm={2}>
+					        {labels[field]}
+					      </Col>
+					      <Col sm={10}>
+					        <FormControl type={field} placeholder={field} />
+					      </Col>
+					    </FormGroup>))}
 
-	    <label for="country">Country</label>
-	    <select id="country" name="country">
-	      <option value="australia">Australia</option>
-	      <option value="canada">Canada</option>
-	      <option value="usa">USA</option>
-	    </select>
-	  
-	    <input type="submit" value="Submit"/>
-	  </form>
-		</div>
-	</Modal>
-  	}
+		        	{this.contentPanel()}
+				    </Form>
+		      </Modal.Body>
+
+		      <Modal.Footer>
+		        <Button onClick={() => this.props.closeModal()}>Close</Button>
+		        <Button bsStyle="primary">Save changes</Button>
+		      </Modal.Footer>
+
+	    	</Modal>
+	  </div>)
+	} 
 }
 
 export default UserModal
